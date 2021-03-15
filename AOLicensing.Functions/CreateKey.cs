52022c4cee7efbb056efb119a881411bb5edec7e
Adam O'Neil <adamosoftware@gmail.com>
2021-03-15 11:47:06 -0400

    Add project files.

---
+++ AOLicensing.Functions/CreateKey.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AOLicensing.Functions
+{
+    public static class CreateKey
+    {
+        [FunctionName("CreateKey")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log, ExecutionContext context)
+        {
+            // generate key for a product
+
+            // save to storage account
+
+            // send to user
+        }
+    }
+}
