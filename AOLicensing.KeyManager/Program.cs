49b0bc9996885c0d6c975959148790d33459702a
Adam O'Neil <adamosoftware@gmail.com>
2021-03-15 16:13:05 -0400

    Desktop package info, started on console app

---
+++ AOLicensing.KeyManager/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+
+namespace AOLicensing.KeyManager
+{
+    class Program
+    {
+        private static HttpClient _client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            // create: create a new key for an email address (after someone purchases) and send to that person
+
+            // query: find a key belonging to an email address (help someone recover)
+
+            // verify: test validation of a key as a sanity check
+
+        }
+    }
+}
