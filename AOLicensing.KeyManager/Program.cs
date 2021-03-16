b88590d8a568b83cc76d20f7d813a86d229ffbb8
Adam O'Neil <adamosoftware@gmail.com>
2021-03-16 13:04:06 -0400

    key manager app working

--- AOLicensing.KeyManager/Program.cs
+++ AOLicensing.KeyManager/Program.cs
@@ -1,43 +1,66 @@
 ﻿using AOLicensing.Shared;
-using CommandLine;
+using Microsoft.Extensions.Configuration;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AOLicensing.KeyManager
 {
+    internal enum Action
+    {
+        Query,
+        Create,
+        Validate
+    }
+
     class Program
     {
         static async Task Main(string[] args)
         {
-            var client = new LicensingClient("https://aolicensing.azurewebsites.net");
+            var config = new ConfigurationBuilder().AddUserSecrets("5a664fcb-5e4e-479d-872f-f47dbbb60ade").Build();
+            var client = new LicensingClient("https://aolicensing.azurewebsites.net", config["Codes:Master"]);
+            
+            Console.WriteLine("AOLicensing KeyManager");
 
-            await Parser.Default
-                .ParseArguments<Options>(args)
-                .WithParsedAsync(async options =>
+            Dictionary<string, (string, Action)> actions = new Dictionary<string, (string, Action)>()
+            {
+                ["q"] = ("Query", Action.Query),
+                ["c"] = ("Create Key", Action.Create)
+            };
+
+            do
+            {
+                Console.WriteLine($"Action ({string.Join(", ", actions.Select(kp => $"{kp.Key} = {kp.Value.Item1}"))}");
+                var actionKey = Console.ReadLine();
+                var action = actions[actionKey];
+
+                Console.WriteLine($"{action.Item1} - customer email:");
+                var email = Console.ReadLine();
+
+                Console.WriteLine("Product:");
+                var product = Console.ReadLine();
+
+                switch (action.Item2)
                 {
-                    switch (options.Action)
-                    {
-                        case Action.Create:
-                            await CreateKeyAsync(client, options);
-                            break;
+                    case Action.Create:
+                        await CreateKeyAsync(client, product, email);
+                        break;
 
-                        case Action.Query:
-                            await QueryKeyAsync(client, options);
-                            break;
+                    case Action.Query:
+                        await QueryKeyAsync(client, product, email);
+                        break;
+                }
+            } while (true);
 
-                        case Action.Validate:
-                            // this is for interactive validation. I don't see this being used very much
-                            break;
-                    }
-                });                
         }
 
-        private async static Task QueryKeyAsync(LicensingClient client, Options options)
+        private async static Task QueryKeyAsync(LicensingClient client, string product, string email)
         {
             var result = await client.QueryAsync(new Shared.Models.CreateKey()
             {
-                Product = options.Product,
-                Email = options.Email
+                Product = product,
+                Email = email
             });
 
             Console.WriteLine($"Found {result.Count} result(s):");
@@ -45,17 +68,19 @@ namespace AOLicensing.KeyManager
             {
                 Console.WriteLine(" " + item);
             }
+            Console.WriteLine();
         }
 
-        private async static Task CreateKeyAsync(LicensingClient client, Options options)
+        private async static Task CreateKeyAsync(LicensingClient client, string product, string email)
         {
             var result = await client.CreateKeyAsync(new Shared.Models.CreateKey()
             {
-                Email = options.Email,
-                Product = options.Product
+                Email = email,
+                Product = product
             });
 
-            Console.WriteLine($"Created key {result.Key} for {result.Email}");
+            Console.WriteLine($"Created {result.Product} key {result.Key} for {result.Email}");
+            Console.WriteLine();
         }
     }
 }
